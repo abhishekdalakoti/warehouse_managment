0fb83412e1118e101718c1d4f76ec0b886f8c2e0
abhishek dalakoti <abhishek.dalakoti@octro.com>
2020-06-03 12:08:26 +0530

    test_cases

--- backend/test/apitest.js
+++ backend/test/apitest.js
@@ -15,4 +15,20 @@ describe('Container', () => {
              });
           });
      });
+     describe('/POST container', () => {
+        it('it should not POST a container without label and hold_id field', (done) => {
+            let request = {
+                label:"CONTAINER 123",
+                hold_id:1,
+            }
+          chai.request(request)
+              .post('/container')
+              .send(request)
+              .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                done();
+              });
+        });
    });
+});
